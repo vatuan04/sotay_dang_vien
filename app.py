4562dc3488ed248cc16ac790d40a2d3bad778ad2
voanhtuan697 <anhtuanvo697@gmail.com>
2025-09-29 19:39:28 +0700

    sqlite sang portgre

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ from datetime import datetime, timezone, timedelta
 from flask_migrate import Migrate   
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'change_me_in_prod')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///instance/sotay.db')
+DATABASE_URL = os.environ.get("DATABASE_URL")  # lấy tự động từ Render
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
